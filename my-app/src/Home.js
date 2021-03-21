fadf41612aa7121e0462cf2887285d13e79c1bc7
Whitney Omoruyi <54600423+womo22@users.noreply.github.com>
2021-03-21 16:34:25 -0500

    Update Home.js

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -15,6 +15,22 @@ function Home() {
 class Home extends Component {
     constructor(props){
         super(props);
+         render(){
+        return(
+            <div>
+            <section id = "colorlib-hero" className="js-fullheight" data-section="home">
+            <div className= "flex-slider js-fullheight">
+            <ul className = "slides">
+            <li style = {{ backgroundImage: //'url ()'}} //Need to put url image of the homepage
+            <div className= "overlay" />
+            <div className = "container-fluid">
+            <div className= "row">
+            <div className= "col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+            <div className = "slider-inner-text js-fullheight">
+            <div className= "desc">
+            <w0> className= Welcome! <br 
+            
+            
     }
 
     render(){
